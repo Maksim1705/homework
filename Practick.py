286d625d2e32f9768cbf29605820e5a033ca5dfe
Максим <maksidrisoviska@gmail.com>
2024-10-31 22:59:14 +0300

    31.10

---
+++ Practick.py
@@ -0,0 +1,15 @@
+Course = "Course:Python"
+Total_tasks = "Total_tasks: 12"
+Hours_spent = "Hours_spent: 1.5"
+AverageExecutionTime = "AverageExecutionTime"
+Time= 1.5/12
+print(Course , Total_tasks , Hours_spent , AverageExecutionTime,Time)
+
+name = "Name:Maksim"
+print(name)
+age = "Age:25"
+print(age)
+new_age = "New Age:26"
+print(new_age)
+name= "is_student: True"
+print(name )
